a812f7fb13a498a3887a4fbcc0c16613ea9be6f5
Anthony Girard <anthony.girard.perso@hotmail.com>
2024-11-21 15:05:38 +0100

    feat(front): Creation login component

---
+++ src/component/Sidebar/Sidebar.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+            {/* Sidebar - Brand */}
+            <a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
+                <div className="sidebar-brand-icon rotate-n-15">
+                    <i className="fas fa-laugh-wink"></i>
+                </div>
+                <div className="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+            </a>
+
+            <hr className="sidebar-divider my-0"/>
+
+            {/* Nav Item - Dashboard */}
+            <li className="nav-item active">
+                <a className="nav-link" href="/">
+                    <i className="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span></a>
+            </li>
+
+            <hr className="sidebar-divider"/>
+
+            {/* Heading */}
+            <div className="sidebar-heading">
+                Interface
+            </div>
+
+            {/* Nav Item - Pages Collapse Menu */}
+            <li className="nav-item">
+                <a className="nav-link" href="#" data-toggle="collapse" data-target="#collapseTwo"
+                   aria-expanded="true" aria-controls="collapseTwo">
+                    <i className="fas fa-fw fa-cog"></i>
+                    <span>Components</span>
+                </a>
+                <div id="collapseTwo" className="collapsed" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div className="bg-white py-2 collapse-inner rounded">
+                        <h6 className="collapse-header">Custom Components:</h6>
+                        <a className="collapse-item" href="buttons.html">Buttons</a>
+                        <a className="collapse-item" href="cards.html">Cards</a>
+                    </div>
+                </div>
+            </li>
+
+        </ul>
+    )
+}
+
+export default Sidebar;
