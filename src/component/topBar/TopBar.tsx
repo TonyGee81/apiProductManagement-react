56cdbe56a3ac66222873a87764f72ef4be06fa19
Anthony Girard <anthony.girard.perso@hotmail.com>
2024-11-27 12:08:53 +0100

    feat(front): change display field

--- src/component/topBar/TopBar.tsx
+++ src/component/topBar/TopBar.tsx
@@ -5,12 +5,14 @@ import Loader from "../loader/Loader";
 const TopBar = () => {
 
     type Tdata = {
-        username: string,
+        firstName: string,
+        lastName: string,
         email: string,
     }
 
     const [userInfo, setUserInfo] = useState({
-        username: "",
+        firstName: "",
+        lastName: "",
         email: ""
     });
 
@@ -23,8 +25,9 @@ const TopBar = () => {
         api.then((data: Tdata) => {
             setIsLoading(false);
             setUserInfo({
+                firstName: data.firstName,
+                lastName: data.lastName,
                 email: data.email,
-                username: data.username
             });
         })
     },[]);
@@ -43,7 +46,7 @@ const TopBar = () => {
                     <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         {isLoading ? <Loader text={''}/> : null}
-                        <span className="mr-2 d-none d-lg-inline text-gray-600 small">- {userInfo.username} -</span>
+                        <span className="mr-2 d-none d-lg-inline text-gray-600 small">- {userInfo.firstName} -</span>
                     </a>
                     <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                          aria-labelledby="userDropdown">
